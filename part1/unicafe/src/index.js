baa7b60a74f652ba5b5d201a8c47a0e996d0aaeb
Tan Nguyen <htltan2003@gmail.com>
2020-06-20 21:12:58 +0300

    1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,7 +2,15 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const Statistic = (props) => {
-  if(props.sum == 0) {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  if(props.sum === 0) {
     return (
       <div>
         <h1>Statistic</h1>
@@ -13,13 +21,21 @@ const Statistic = (props) => {
   return (
     <div>
       <h1>Statistic</h1>
-      <p>all {props.sum}</p>
-      <p>average {props.avg}</p>
-      <p>positive {props.positiveRate}%</p>
+      <Statistic text="good" value={props.good}/>
+      <Statistic text="neutral" value={props.neutral}/>
+      <Statistic text="bad" value={props.bad}/>
+      <Statistic text="all" value={props.sum}/>
+      <Statistic text="average" value={props.avg}/>
+      <Statistic text="positive" value={props.positiveRate}/>
     </div>
   )
 }
 
+const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+)
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -27,16 +43,21 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const sum = good  + neutral + bad
   const avg = (good - bad) / sum || 0
-  const positiveRate = good / sum || 0
+  const positiveRate = (good / sum) + "%" || 0
 
   return (
     <div>
       <div>
         <h1>give feedback</h1>
-        <button onClick = {() => setGood(good + 1)}>good</button>
-        <button onClick = {() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick = {() => setBad(bad + 1)}>bad</button>
-        <Statistic sum={sum} avg={avg} positiveRate={positiveRate}/>
+        <Button onClick = {() => setGood(good + 1)} text="good"/>
+        <Button onClick = {() => setNeutral(neutral + 1)} text="neutral" />
+        <Button onClick = {() => setBad(bad + 1)} text="bad"/>
+        <Statistics good={good} 
+                    neutral={neutral} 
+                    bad={bad} 
+                    sum={sum} 
+                    avg={avg} 
+                    positiveRate={positiveRate}/>
       </div>
     </div>
   )
