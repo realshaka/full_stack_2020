7ff63df518a6c4b06a9978714b21404efcff3737
Tan Nguyen <htltan2003@gmail.com>
2020-06-21 22:44:25 +0300

    1.14

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -5,19 +5,31 @@ const App = (props) => {
   const [selected, setSelected] = useState(0)
   const points = Array.apply(null, new Array(10)).map(Number.prototype.valueOf,0);
   const [copy, setPoints] = useState([...points])
+  const [maxIndex, setMaxIndex] = useState(0)
   const voteClick = () => {
     copy[selected] += 1
-    let newCopy = [...copy]
     setPoints([...copy])
     console.log(copy[selected])
+    var max = copy.indexOf(Math.max(...copy))
+    setMaxIndex(max)
+    console.log(max)
   }
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {copy[selected]} votes</p>
       <button onClick={() => setSelected(Math.floor(Math.random() * 6))}>next anecdote</button>
       <button onClick={voteClick}>vote</button>
+
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <p>{props.anecdotes[maxIndex]}</p>
+        <p>has {copy[maxIndex]} votes</p>
+      </div>
     </div>
+
+   
   )
 }
 
