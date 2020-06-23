6cb9015e20712c4b41d9aa32f9c762588feab3a4
Tan Nguyen <htltan2003@gmail.com>
2020-06-23 23:12:57 +0300

    2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -23,7 +23,7 @@ const Part = ({part}) => {
   )
 }
 
-const Content = ({ course}) => {
+const Content = ({course}) => {
   return (
     <div>
       {course.parts.map(part =>
@@ -36,41 +36,63 @@ const Content = ({ course}) => {
 const Course = ({course}) => {
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      {course.map(c => 
+        <div>
+          <Header course={c} />
+          <Content course={c} />
+          <Total course={c} />
+        </div>
+      )}
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Course course={courses} />
 }
 ReactDOM.render(<App />, document.getElementById('root'))
