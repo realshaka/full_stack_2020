c1df057a126cca2643734af78a44f7e8c0617a8e
Tan Nguyen <htltan2003@gmail.com>
2020-07-01 18:18:55 +0300

    phonebook backend 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,37 +2,47 @@ const express = require('express')
 const app = express()
 
 const persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    },]
+	{
+		"name": "Arto Hellas",
+		"number": "040-123456",
+		"id": 1
+	},
+	{
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523",
+		"id": 2
+	},
+	{
+		"name": "Dan Abramov",
+		"number": "12-43-234345",
+		"id": 3
+	},
+	{
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122",
+		"id": 4
+	},]
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+	res.json(persons)
 })
 
 app.get('/info/', (req, res) => {
-    const date = new Date()
-    const text = `Phonebook has info of ${persons.length} people 
+	const date = new Date()
+	const text = `Phonebook has info of ${persons.length} people 
     \n${date}`
-    console.log(text)
-    res.end(text)
+	console.log(text)
+	res.end(text)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(person => person.id === id)
+	if(person==null) {
+		res.status(404).end()
+	}
+	console.log(person)
+	res.json(person)
 })
 
 const port = 3001
