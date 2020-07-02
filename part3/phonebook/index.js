7b1ec339f445af9a970d1dbbb994077d62cae180
Tan Nguyen <htltan2003@gmail.com>
2020-07-02 15:14:01 +0300

    phonebook backend 3.7, 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
+const { json } = require('express')
 
+morgan.token('body', function (req, res) { if (!req.body) {return} else {return JSON.stringify(req.body)}})
+morgan.token('tiny', ':method :url :status :res[content-length] - :response-time ms :body')
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
 
 var persons = [
 	{
@@ -41,7 +47,7 @@ app.get('/info/', (req, res) => {
 app.get('/api/persons/:id', (req, res) => {
 	const id = Number(req.params.id)
 	let person = persons.find(person => person.id === id)
-	if(person==null) {
+	if (person == null) {
 		res.status(404).end()
 	}
 	console.log(person)
@@ -49,34 +55,36 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
+	const id = Number(req.params.id)
+	persons = persons.filter(person => person.id !== id)
 	console.log(persons)
-  res.status(204).end()
+	res.status(204).end()
 })
 
 app.post('/api/persons', (req, res) => {
+
 	const body = req.body
 	if (!body.name | !body.number) {
-    return res.status(400).json({ 
-      error: 'content missing' 
-    })
+		return res.status(400).json({
+			error: 'content missing'
+		})
 	}
 
-	const nameNotUnique = persons.some(person => person.name === body.name) 
+	const nameNotUnique = persons.some(person => person.name === body.name)
 	if (nameNotUnique) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
+		return res.status(400).json({
+			error: 'name must be unique'
+		})
 	}
 
 	const person = body
-	const id = Math.floor(Math.random() * Math.floor(2^9))
+	const id = Math.floor(Math.random() * Math.floor(2 ^ 9))
 	person.id = id
-	console.log(person)
+	//console.log(person)
 	persons = persons.concat(person)
-	res.json(person)
-	console.log(persons)
+	res.status(200)
+	res.json(req.body)
+	//console.log(persons)
 })
 
 const port = 3001
