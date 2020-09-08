98f9d72abf5b06a1ce79c430de6d6b1d1c2e3c8f
Tan Nguyen <htltan2003@gmail.com>
2020-09-09 01:43:28 +0300

    anecdotes 7.1-7.3

---
+++ part7/routed-anecdotes/src/components/Menu.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link,
+} from "react-router-dom"
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5
+  }
+  return (
+    <div>
+      <Link style={padding} to="/">anecdotes</Link>
+      <Link style={padding} to="/create">create new</Link>
+      <Link style={padding} to="/about">about</Link>
+    </div>
+  )
+}
+
+export default Menu
